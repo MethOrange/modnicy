e4ebf9a363860ce041f7faa2e734fd3464008fef
Modnic <name>
2020-11-19 13:06:16 +0200

    shop 1.1.8

--- modnic/settings.py
+++ modnic/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '*c86bm6njv+b*ch)bmy)o&wf0^gwfa@t%5p1*^#r+dok6t+vl('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['modnicy.com.ua','127.0.0.1']
+ALLOWED_HOSTS = ['modnicy.com.ua','127.0.0.1','195.54.162.216']
 
 
 # Application definition
