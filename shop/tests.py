e9401d1f746e72ec82e52f467cb440300394ecf6
Modnic <name>
2020-11-19 14:18:28 +0200

    shop 1.1.13

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SearchFormTestCase(TestCase):
+    def test_empty_get(self):
+        response = self.client.get('/en/dev/search/', HTTP_HOST='modnicy.com.ua:8000')
+        self.assertEqual(response.status_code, 200)
